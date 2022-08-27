57bd38dd62790e2f03b0436a3de44cead0d3e74a
Pulkit <pulkitnsit@gmail.com>
2022-08-28 03:36:16 +0530

    Add lp_lama app

--- backend/abstract_classes/abstract_exchange.py
+++ backend/abstract_classes/abstract_exchange.py
@@ -1,12 +1,10 @@
 import logging
-
-from web3 import Web3
+from datetime import datetime
 
 from abstract_classes.abstract_contract import AbstractContract
 # from common.utils import get_deadline
 from covalent import Covalent
 from lp_position import LpPosition
-from datetime import datetime
 
 logger = logging.getLogger(__name__)
 
@@ -37,6 +35,20 @@ class AbstractExchange(AbstractContract):
         reward_earned, last_updated_block, _ = self.contract.functions.poolInfo(int(pool_id)).call(block_identifier=block_identifier)
         return reward_earned
 
+    def get_tvl(self, pool_id: int, block_identifier='latest'):
+        end_datetime = datetime.fromtimestamp(self.w3.eth.getBlock(block_identifier).timestamp)
+        block_date = end_datetime.strftime("%Y-%m-%d")
+        lp_token0_price, token0_decimal = self.covalent.get_token_price_and_decimal_at_date(
+            self.lp_address(pool_id).token0_addr, block_date)
+        lp_token1_price, token1_decimal = self.covalent.get_token_price_and_decimal_at_date(
+            self.lp_address(pool_id).token1_addr, block_date)
+        reserve0, reserve1, _ = self.lp_address(pool_id).contract.functions.getReserves().call(
+            block_identifier=block_identifier)
+
+        lp_price = (lp_token0_price * reserve0 / 10 ** token0_decimal) + (
+                    lp_token1_price * reserve1 / 10 ** token1_decimal)
+        return lp_price
+
     def get_reward_earned_between_blocks(self, pool_id, start_block, end_block):
         start_reward = self.get_pool_info(pool_id, start_block)
         end_reward = self.get_pool_info(pool_id, end_block)
@@ -46,11 +58,9 @@ class AbstractExchange(AbstractContract):
         reward_price, reward_decimal = self.covalent.get_token_price_and_decimal_at_date(self.REWARD_ADDRESS, reward_price_date)
         total_reward = reward_earned * reward_price / 10**reward_decimal
 
-        lp_token0_price, token0_decimal = self.covalent.get_token_price_and_decimal_at_date(self.lp_address(pool_id).token0_addr, reward_price_date)
-        lp_token1_price, token1_decimal = self.covalent.get_token_price_and_decimal_at_date(self.lp_address(pool_id).token1_addr, reward_price_date)
-        reserve0, reserve1, _ = self.lp_address(pool_id).contract.functions.getReserves().call(block_identifier=end_block)
-        lp_supply = self.lp_address(pool_id).contract.functions.balanceOf(self.ADDRESS).call(block_identifier=end_block)
-        lp_price = (lp_token0_price * reserve0 / 10**token0_decimal) + (lp_token1_price * reserve1 / 10**token1_decimal)
+        lp_supply = self.lp_address(pool_id).contract.functions.balanceOf(self.ADDRESS).call(
+            block_identifier=end_block)
+        lp_price = self.get_tvl(pool_id, end_block)
 
         reward_apr = total_reward * lp_supply * 100 * 365 / (lp_price * 1e12)
         print(reward_apr)
--- backend_server/backend_server/settings.py
+++ backend_server/backend_server/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from corsheaders.defaults import default_headers
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-u+x8pxl(4m=yjbktqsh42w)6dd8n$k@)jrp5d$o7djab*!jz*&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'lp_lama',
+    'rest_framework',
+    'rest_framework_swagger',
 ]
 
 MIDDLEWARE = [
@@ -47,7 +51,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
+
+CORS_ALLOW_HEADERS = default_headers + (
+    'pragma',
+)
 
 ROOT_URLCONF = 'backend_server.urls'
 
