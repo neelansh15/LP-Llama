940fc9478c69fb42cf450ec1a039031e281f5af4
Pulkit <pulkitnsit@gmail.com>
2022-08-28 12:14:35 +0530

    Update il calculation

--- backend_server/lp_lama/analytics/storage/il_computer.py
+++ backend_server/lp_lama/analytics/storage/il_computer.py
@@ -1,2 +1,10 @@
-def compute_il():
-    pass
+def calculate_il(p1, p2, p3, p4):
+    initial_price = p2 / p1
+    future_price = p4 / p3
+    price_increase = abs(future_price - initial_price) / initial_price
+    il = abs((2 * (price_increase ** 0.5) / (1 + price_increase)) - 1)
+    return il
+
+
+if __name__ == '__main__':
+    print(calculate_il(1, 100, 1, 110))
